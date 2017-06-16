234bf8a88bb140d7cbbbd087a86ff0a890a59b83
Xiaomei <xiaomei.wang07@gmail.com>
2017-06-15 22:23:44 -0700

    test icon

--- app/src/main/java/com/example/android/vigi/SearchResultDetailActivity.java
+++ app/src/main/java/com/example/android/vigi/SearchResultDetailActivity.java
@@ -6,28 +6,26 @@ package com.example.android.vigi;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.media.Image;
-import android.net.Uri;
-import android.support.v4.app.ShareCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.database.sqlite.SQLiteDatabase;
-import android.widget.ImageView;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-
-import java.io.InputStream;
-
-import com.example.android.vigi.utils.NewsUtils;
 import com.example.android.vigi.data.NewsSearchContract;
 import com.example.android.vigi.data.NewsSearchDBHelper;
+import com.example.android.vigi.utils.NewsUtils;
+
+import java.io.InputStream;
 
 public class SearchResultDetailActivity extends AppCompatActivity {
 
